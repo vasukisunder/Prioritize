75eaa97d2e11ffcd0a049cba0c0fef356e934ecb
vasuki <sunder.vasuki@gmail.com>
2018-02-17 01:57:11 -0800

    updates

--- routes/add.js
+++ routes/add.js
@@ -1,12 +1,16 @@
-var item_data = require('../itemData.json');
+var data = require('../data.json');
 
 exports.addItem = function(req, res) {  
 	// Your code goes here
 	var newItem =  {
-		item_name: req.query.item_name,
-		item_price: req.query.item_price,
-		item_priority: req.query.item_priority
+		name: req.query.name,
+		price: req.query.price,
+		priority: req.query.radio
 	};
-	item_data.itemDatabase.push(newItem);
-	res.render('index', item_data);
+
+	console.log(newItem);
+	
+	data.Items.push(newItem);
+	res.render('index', data);
 }
+
